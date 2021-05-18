5fe03ec7d9902defea3647f7238c7bf4ce918319
AparnaPatil28 <76910620+AparnaPatil28@users.noreply.github.com>
2021-05-18 10:34:01 +0530

    Update App.java

--- App.java
+++ App.java
@@ -20,6 +20,7 @@ public static void main(String[] args) {
 	WebDriver driver = new FirefoxDriver(firefoxOptions); //Creating an object of FirefoxDriver	 
 	
 	//firefoxOptions.addArguments("--headless");
+	firefoxOptions.setHeadless(true);
 	
 	//System.setProperty("webdriver.chrome.driver","C:\\Users\\japat\\Desktop\\Aparna\\chromedriver.exe");
 	//System.setProperty("webdriver.chrome.driver","/home/ubuntu/selenium/chromedriver");
