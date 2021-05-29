dce382f6510c7e0e49dabc8ce08ebfb78adf77da
AparnaPatil28 <76910620+AparnaPatil28@users.noreply.github.com>
2021-05-29 06:03:30 +0530

    Update App.java

--- src/main/java/com/mycompany/app/addressbook_selenium/App.java
+++ src/main/java/com/mycompany/app/addressbook_selenium/App.java
@@ -3,10 +3,10 @@ package com.mycompany.app.addressbook_selenium;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.firefox.FirefoxOptions;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.Test;
 
 public class App
